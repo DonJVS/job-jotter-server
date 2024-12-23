dd59fb51107f9c0f07f239ce5aaed41f02c5f1c2
Justin Mejias <justin17mejias@gmail.com>
2024-12-22 21:00:33 -0500

    Defined SECRET_KEY in oauthRouter

--- routes/oauthRouter.js
+++ routes/oauthRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
 const { google } = require("googleapis");
 const db = require("../db");
 const {
